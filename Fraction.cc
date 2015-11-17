29cc350c6a0c91c420261f1b9969d5ab21ddacf3
Shaobo Liu <sliu36@stevens.edu>
2015-11-17 14:11:40 -0500

    add
    constructor

--- Fraction.cc
+++ Fraction.cc
@@ -3,8 +3,10 @@ private:
 	int num, den;
 
 public:
-	Fraction(int n, int d) {}
-
+    Fraction(int n, int d):num(n),den(d) {
+    
+    }
+    
 
 	friend operator +(const Fraction& a, const Fraction& b) {
 
